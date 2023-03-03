e1ffef1f9a8563479957161bb836aae60bb845d6
mrganmak <mrganmak@mail.ru>
2023-03-03 05:47:13 +0300

    PingCommand: Добавлена локализация для текста

--- src/commands/PingCommand.ts
+++ src/commands/PingCommand.ts
@@ -2,6 +2,9 @@ import { CommandInteraction } from "discord.js";
 import { Discord, Slash } from "discordx";
 import { ECommandsIds, ELocalizationsLanguages } from "../enum.js";
 import { getAllLocalizationsForCommandProperty, getLocalizationForCommand } from "../localizations/commands/index.js";
+import { getLocalizationForText } from "../localizations/texts/index.js";
+import EDefaultTextLocalization from "../localizations/texts/list/default.js";
+import { getGuildLanguage } from "../localizations/index.js";
 
 const { name, description } = getLocalizationForCommand(ECommandsIds.PING, ELocalizationsLanguages.EN);
 
@@ -12,10 +15,17 @@ class PingCommand {
 		description,
 		nameLocalizations: getAllLocalizationsForCommandProperty(ECommandsIds.PING, 'name', [ELocalizationsLanguages.EN]),
 		descriptionLocalizations: getAllLocalizationsForCommandProperty(ECommandsIds.PING, 'description', [ELocalizationsLanguages.EN]),
-		guilds: [process.env.TEST_GUILD_ID],
 		dmPermission: false
 	})
-	async test(interaction: CommandInteraction) {
-		interaction.reply({ content: `🏓 | Задержка апи: ${Math.round(interaction.client.ws.ping)}ms`, ephemeral: true });
+	async ping(interaction: CommandInteraction) {
+		if (!interaction.guild) return;
+
+		interaction.reply({
+			content: getLocalizationForText(
+				EDefaultTextLocalization.PING_COMMAND_MESSAGE_TEXT,
+				await getGuildLanguage(interaction.guild.id)).replace('{ping}', String(Math.round(interaction.client.ws.ping))
+			),
+			ephemeral: true
+		});
 	}
 }
