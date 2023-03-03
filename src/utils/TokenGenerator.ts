c0d2c830544c06f6d1e1eafbc0223c73f570616d
mrganmak <mrganmak@mail.ru>
2023-03-03 05:49:11 +0300

    TokenGenerator: Добавлен метод для генерации АПИ-ключей

--- src/utils/TokenGenerator.ts
+++ src/utils/TokenGenerator.ts
@@ -1,5 +1,6 @@
 import { generateApiKey } from "generate-api-key";
 import GuildsIdentifiersBase from "../Databases/bases_list/GuildsIdentifiersBase.js";
+import { createHash } from "crypto";
 
 export default class TokenGenerator {
 	public static async createTokenForGuild(): Promise<string> {
@@ -9,4 +10,18 @@ export default class TokenGenerator {
 		if (await database.getByToken(Array.isArray(token) ? token[0] : token)) return await this.createTokenForGuild();
 		return (Array.isArray(token) ? token[0] : token);
 	}
+
+	public static async createAPIKey(): Promise<APIKey> {
+		const database = new GuildsIdentifiersBase();
+		const key = generateApiKey({ method: 'string', min: 45, max: 60 });
+		const keyHash = createHash('sha512').update(Array.isArray(key) ? key[0] : key).digest('hex');
+
+		if (await database.getByAPIKey(keyHash)) return await this.createAPIKey();
+		return { key: (Array.isArray(key) ? key[0] : key), hash: keyHash };
+	}
+}
+
+interface APIKey {
+	key: string;
+	hash: string;
 }
