650edc6aaf425593e0922949ad9433b16c72fce6
mrganmak <mrganmak@mail.ru>
2023-03-04 04:35:08 +0300

    Конфиг дополнен новыми значениями
    1. userConfirmationInteractionButtonsSettings для кнопок в системе подтверждения от пользователя
    2. buttonsPanelsSettings для настройки панели кнопок
    3. ticketsSettingsSelectMenuComponents для селект меню в настройке тикетов

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,32 @@
+import { ButtonStyle } from "discord.js";
+import { IUserConfirmationInteractionButtonSettings } from "./modules/interactions/UserConfirmation.js";
+import ETextsLocalizationsIds from "./localizations/texts/types/ETextsLocalizationsIds.js";
+import { TButtonsPanelsSettings } from "./modules/interactions/ButtonsPanel.js";
+import { EButtosPanelSSettingsIds } from "./enum.js";
+import { TSelectMenuOptionsWithLocalizations } from "./types/types.js";
+
 export const devMode = true;
+
+export const userConfirmationInteractionButtonsSettings: Array<IUserConfirmationInteractionButtonSettings> = [
+	{
+		label: ETextsLocalizationsIds.USER_CONFIRMATION_BUTTON_NO,
+		style: ButtonStyle.Danger,
+		customId: 'deny'
+	},
+	{
+		label: ETextsLocalizationsIds.USER_CONFIRMATION_BUTTON_YES,
+		style: ButtonStyle.Success,
+		customId: 'confirm'
+	}
+];
+
+export const buttonsPanelsSettings: TButtonsPanelsSettings = { }
+
+export const ticketsSettingsSelectMenuComponents: TSelectMenuOptionsWithLocalizations = [
+	{
+		label: ETextsLocalizationsIds.TICKETS_SETTINGS_CHANGE_CATEGORY_LABEL,
+		description: ETextsLocalizationsIds.TICKETS_SETTINGS_CHANGE_CATEGORY_DESCRIPTION,
+		value: 'change_category'
+	}
+];
+
