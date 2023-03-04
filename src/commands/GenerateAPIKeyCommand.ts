5ca6f40eb87bdb8162e0846598dfdd0e4db60836
mrganmak <mrganmak@mail.ru>
2023-03-04 04:32:09 +0300

    Добавлено подтверждение от пользователя

--- src/commands/GenerateAPIKeyCommand.ts
+++ src/commands/GenerateAPIKeyCommand.ts
@@ -5,8 +5,9 @@ import { getAllLocalizationsForCommandProperty, getLocalizationForCommand } from
 import TokenGenerator from "../utils/TokenGenerator.js";
 import { getGuildLanguage } from "../localizations/index.js";
 import { getLocalizationForText } from "../localizations/texts/index.js";
-import EDefaultTextLocalization from "../localizations/texts/list/default.js";
 import GuildsIdentifiersBase from "../Databases/bases_list/GuildsIdentifiersBase.js";
+import getUserConfirmation from "../modules/interactions/UserConfirmation.js";
+import ETextsLocalizationsIds from "../localizations/texts/types/ETextsLocalizationsIds.js";
 
 const { name, description } = getLocalizationForCommand(ECommandsIds.GENERATE_API_KEY, ELocalizationsLanguages.EN);
 
@@ -23,20 +24,34 @@ class GenerateAPIKeyCommand {
 		if (!interaction.guild) return;
 		await interaction.deferReply({ ephemeral: true });
 		const base = new GuildsIdentifiersBase();
-
-		const APIKey = await TokenGenerator.createAPIKey();
 		const guildLanguage = await getGuildLanguage(interaction.guild.id);
 
+		if ((await base.getByGuildId(interaction.guild.id))?.APIKey) {
+			const userConfirmation = await getUserConfirmation(
+				interaction,
+				{
+					content: getLocalizationForText(
+						ETextsLocalizationsIds.GENERATE_API_KEY_RESET_WARNING_TEXT,
+						guildLanguage
+					),
+					language: guildLanguage
+				}	
+			);
+
+			if (userConfirmation == 'deny') return await interaction.editReply({ content: getLocalizationForText(ETextsLocalizationsIds.GENERATE_API_KEY_RESET_CANCELED_TEXT, guildLanguage) });
+		}
+
+		const APIKey = await TokenGenerator.createAPIKey();
 		const embed = new EmbedBuilder();
 		embed
-			.setTitle(getLocalizationForText(EDefaultTextLocalization.GENERATE_API_KEY_EMBED_TITLE, guildLanguage))
-			.setDescription(getLocalizationForText(EDefaultTextLocalization.GENERATE_API_KEY_EMBED_DESCRIPTION, guildLanguage))
+			.setTitle(getLocalizationForText(ETextsLocalizationsIds.GENERATE_API_KEY_EMBED_LABLE, guildLanguage))
+			.setDescription(getLocalizationForText(ETextsLocalizationsIds.GENERATE_API_KEY_EMBED_DESCRIPTION, guildLanguage))
 			.setFields({
-				name: getLocalizationForText(EDefaultTextLocalization.GENERATE_API_KEY_EMBED_KEY_FIELD_NAME, guildLanguage),
+				name: getLocalizationForText(ETextsLocalizationsIds.GENERATE_API_KEY_EMBED_KEY_FIELD_NAME, guildLanguage),
 				value: APIKey.key
 			});
 
 		await base.addAPIKeyForGuild(interaction.guild.id, APIKey.hash);
-		await interaction.editReply({ embeds: [embed] });
+		await interaction.editReply({ content: '', embeds: [embed] });
 	}
 }
