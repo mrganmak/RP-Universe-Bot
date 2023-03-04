f84c1abd5b658a9fde043cd130a7f7e048693a23
mrganmak <mrganmak@mail.ru>
2023-03-04 04:36:56 +0300

    Обновлена тестовая команда

--- src/commands/TestCommand.ts
+++ src/commands/TestCommand.ts
@@ -1,7 +1,9 @@
-import { CommandInteraction } from "discord.js";
-import { Discord, Slash } from "discordx";
+import { ApplicationCommandOptionType, CommandInteraction } from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
 import { ECommandsIds, ELocalizationsLanguages } from "../enum.js";
 import { getAllLocalizationsForCommandProperty, getLocalizationForCommand } from "../localizations/commands/index.js";
+import { createHash } from "crypto";
+import GuildsIdentifiersBase from "../Databases/bases_list/GuildsIdentifiersBase.js";
 
 const { name, description } = getLocalizationForCommand(ECommandsIds.TEST, ELocalizationsLanguages.EN);
 
@@ -15,7 +17,22 @@ class TestCommand {
 		guilds: [process.env.TEST_GUILD_ID],
 		dmPermission: false
 	})
-	async test(interaction: CommandInteraction) {
-		interaction.reply({ content: 'test', ephemeral: true });
+	async test(
+		@SlashOption({
+			name,
+			description,
+			nameLocalizations: getAllLocalizationsForCommandProperty(ECommandsIds.TEST, 'name', [ELocalizationsLanguages.EN]),
+			descriptionLocalizations: getAllLocalizationsForCommandProperty(ECommandsIds.TEST, 'description', [ELocalizationsLanguages.EN]),
+			required: true,
+			type: ApplicationCommandOptionType.String
+		})
+		test: string,
+		interaction: CommandInteraction
+	) {
+		const base = new GuildsIdentifiersBase();
+		const keyHash = createHash('sha512').update(test).digest('hex');
+		const guildIdentifiers = await base.getByAPIKey(keyHash);
+
+		interaction.reply({ content: (guildIdentifiers ? 'yes' : 'no'), ephemeral: true });
 	}
 }
