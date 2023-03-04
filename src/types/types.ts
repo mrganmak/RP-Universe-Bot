d2981208deee265971b77c65e448df0ff9b7943d
mrganmak <mrganmak@mail.ru>
2023-03-04 04:36:49 +0300

    Добавлены типы для пагенационного селект меню

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,13 @@
+import { APIMessageComponentEmoji } from "discord.js";
+import ETextsLocalizationsIds from "../localizations/texts/types/ETextsLocalizationsIds.js";
+
 export type ValueOf<T> = T[keyof T];
+
+export type TSelectMenuOptionsWithLocalizations = Array<ISelectMenuOptionWithLocalizations>;
+export interface ISelectMenuOptionWithLocalizations {
+	label: ETextsLocalizationsIds;
+	value: string;
+	default?: boolean;
+	description?: ETextsLocalizationsIds;
+	emoji?: APIMessageComponentEmoji;
+}
