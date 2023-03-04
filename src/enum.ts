698b33e2c4b7084983a5c4a2c66bd63bc022d453
mrganmak <mrganmak@mail.ru>
2023-03-04 04:35:23 +0300

    Добавлена id новых команд

--- src/enum.ts
+++ src/enum.ts
@@ -2,10 +2,13 @@ export enum ECommandsIds {
 	TEST,
 	PING,
 	GENERATE_API_KEY,
-	SET_SERVER_LANGUAGE
+	SET_SERVER_LANGUAGE,
+	TICKETS_SETTINGS,
 }
 
 export enum ELocalizationsLanguages {
 	EN = 'en-GB',
 	RU = 'ru'
 }
+
+export enum EButtosPanelSSettingsIds { }
