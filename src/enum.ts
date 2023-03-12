ba167d57c6236a9e7b4b7a0e3c3fd58f20c601f6
mrganmak <mrganmak@mail.ru>
2023-03-12 03:46:23 +0300

    enum: Добавлены ECommandsCategirysIds

--- src/enum.ts
+++ src/enum.ts
@@ -4,6 +4,16 @@ export enum ECommandsIds {
 	GENERATE_API_KEY,
 	SET_SERVER_LANGUAGE,
 	TICKETS_SETTINGS,
+	START,
+	START_TICKETS,
+}
+
+export enum ECommandsCategirysIds {
+	DEVELOPMENT = 'development',
+	ONLY_IN_INITED_GUILDS = 'only_in_inited_guilds',
+	ONLY_IN_NOT_INITED_GUILDS = 'only_in_not_inited_guilds',
+	ONLY_WITH_TICKETS_INITED = 'only_with_tickets_inited',
+	ONLY_WITH_TICKETS_NOT_INITED = 'only_with_tickets_not_inited'
 }
 
 export enum ELocalizationsLanguages {
