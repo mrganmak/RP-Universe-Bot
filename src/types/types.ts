3078f3382f832ee02b302d3df1c3f3cebfe56c94
mrganmak <mrganmak@mail.ru>
2023-03-12 03:40:44 +0300

    types: Добавлен тип ChangeAllTypes

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,6 +2,21 @@ import { APIMessageComponentEmoji } from "discord.js";
 import ETextsLocalizationsIds from "../localizations/texts/types/ETextsLocalizationsIds.js";
 
 export type ValueOf<T> = T[keyof T];
+export type ChangeAllTypes<InstanceType extends Array<any> | Object, FinalType extends any, TargetType extends any = string> = {
+	[Key in keyof InstanceType]: (
+		NonNullable<InstanceType[Key]> extends TargetType ?
+		FinalType :
+		(
+			NonNullable<InstanceType[Key]> extends Array<any> ?
+			ChangeAllTypes<NonNullable<InstanceType[Key]>, FinalType, TargetType> :
+			(
+				NonNullable<InstanceType[Key]> extends Object ?
+				ChangeAllTypes<NonNullable<InstanceType[Key]>, FinalType, TargetType> :
+				InstanceType[Key]
+			)
+		)
+	)
+}
 
 export type TSelectMenuOptionsWithLocalizations = Array<ISelectMenuOptionWithLocalizations>;
 export interface ISelectMenuOptionWithLocalizations {
