da4787f2d90ded24ea93e32aa89100187b53ee49
mrganmak <mrganmak@mail.ru>
2023-03-12 03:38:57 +0300

    init

---
+++ src/modules/tickets/Tickets.ts
@@ -0,0 +1,12 @@
+import { Snowflake } from "discord.js";
+import Ticket from "./classes/Ticket.js";
+
+export class Tickets {
+	private static _tickets: Map<Snowflake, Ticket> = new Map();
+
+	public static async create(): Promise<Ticket> {
+		const ticket = new Ticket();
+		//this._tickets.set();
+		return ticket;
+	}
+}
