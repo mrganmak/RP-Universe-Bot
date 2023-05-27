06b8d0fbbd89d67fe24e8549cb0c0b0059feea45
mrganmak <mrganmak@mail.ru>
2023-05-27 16:44:52 +0300

    entry.bot: Рефакторинг

--- src/entry.bot.ts
+++ src/entry.bot.ts
@@ -5,25 +5,27 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { CommandsIniter, MongoBase } from "./index.js";
 
-const client = new Client({
-	intents: [
-		IntentsBitField.Flags.Guilds,
-		IntentsBitField.Flags.GuildMessages,
-		IntentsBitField.Flags.GuildMessageReactions,
-		IntentsBitField.Flags.GuildMembers,
-		IntentsBitField.Flags.MessageContent,
-	]
-});
+export class Bot {
+	private static _client = new Client({
+		intents: [
+			IntentsBitField.Flags.Guilds,
+			IntentsBitField.Flags.GuildMessages,
+			IntentsBitField.Flags.GuildMessageReactions,
+			IntentsBitField.Flags.GuildMembers,
+			IntentsBitField.Flags.MessageContent,
+		]
+	});
 
-async function start() {
-	CommandsIniter.changeClient(client);
+	public static async start() {
+		CommandsIniter.changeClient(this._client);
 
-	const __filename = fileURLToPath(import.meta.url);
-	const __dirname = dirname(__filename);
-
-	await MongoBase.initBase();
-	await importx(`${__dirname}/{events,commands}/**/**.js`);
-	await client.login(process.env.TOKEN);
+		const __filename = fileURLToPath(import.meta.url);
+		const __dirname = dirname(__filename);
+	
+		await MongoBase.initBase();
+		await importx(`${__dirname}/{events,commands}/**/**.js`);
+		await this._client.login(process.env.TOKEN);
+	}
 }
 
-start();
+Bot.start();
