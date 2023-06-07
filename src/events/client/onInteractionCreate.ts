763dd07646a689dd22e7047086a1361fe62886a8
mrganmak <mrganmak@mail.ru>
2023-06-07 23:57:02 +0300

    onInteractionCreate: Интегрирован токен гильдии
    Теперь гильдии, из которой отправляется взаимодействие, будет присвоен токен, если его не было на момент отправки

--- src/events/client/onInteractionCreate.ts
+++ src/events/client/onInteractionCreate.ts
@@ -1,13 +1,26 @@
 import { Events } from "discord.js";
 import { ArgsOf, Client, Discord, On } from "discordx";
+import { GuildsIdentifiersBase, TokenGenerator } from "../../index.js";
 
 @Discord()
 class onInteractionCreate {
 	@On({ event: Events.InteractionCreate })
-	onInteractionCreate(
+	async onInteractionCreate(
 		[interaction]: ArgsOf<Events.InteractionCreate>,
 		client: Client
 	) {
+		if (interaction.guild) {
+			const database = new GuildsIdentifiersBase();
+			const guildIdentifier = await database.getByGuildId(interaction.guild.id);
+			
+			if (!guildIdentifier?.token) {
+				const token = await TokenGenerator.createTokenForGuild();
+				const guildId = interaction.guild.id;
+		
+				await database.addIdentifier({ token, guildId });
+			}	
+		}
+
 		client.executeInteraction(interaction);
 	}
 }
