db132fc23466802e4fd9dc06a982e480071e14ec
mrganmak <mrganmak@mail.ru>
2023-06-09 23:03:54 +0300

    TicketsCommndInteractions: Фикс ошибки с некорректными данными возврата

--- src/modules/tickets/utils/TicketsCommndInteractions.ts
+++ src/modules/tickets/utils/TicketsCommndInteractions.ts
@@ -137,7 +137,7 @@ export class TicketsCommndInteractions {
 				: null
 			)
 			: null
-		) ?? messageSettings.title;
+		) ?? messageSettings.description;
 		messageSettings.buttonText = (
 			buttonTextField.type === ComponentType.TextInput
 			? (
@@ -146,7 +146,7 @@ export class TicketsCommndInteractions {
 				: null
 			)
 			: null
-		) ?? messageSettings.title;
+		) ?? messageSettings.buttonText;
 
 		return messageSettings;
 	}
