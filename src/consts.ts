59436b102dc5870f214e3781eabf895f5534f020
mrganmak <mrganmak@mail.ru>
2023-12-14 03:00:20 +0300

    consts: Added new constants  for the marker system

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,7 @@
 import { LocalizationsLanguages } from "./index.js";
 
 export const DEFAULT_SERVER_LANGUAGE = LocalizationsLanguages.EN;
+
+export const INTEGRITY_POINTS_MULTIPLE_VALUE = 100;
+export const INTEGRITY_LEVEL_MIN_VALUE = -3000;
+export const INTEGRITY_LEVEL_MAX_VALUE = 1000;
