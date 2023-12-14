18373a3b95821389f449f3f0e4f2f7734f559c92
mrganmak <mrganmak@mail.ru>
2023-12-14 03:08:56 +0300

    ShardsUtils: A function for searching guilds in shards has been added.

--- src/utils/ShardsUtils.ts
+++ src/utils/ShardsUtils.ts
@@ -1,5 +1,9 @@
-import { Client, GuildEmoji } from "discord.js";
+import { Client, Guild, GuildEmoji } from "discord.js";
 
 export function findEmojiInClient(clinet: Client, { id }: { id: string }): GuildEmoji | null {
 	return (clinet.emojis.cache.get(id) ?? null);
+}
+
+export function findGuildInClient(clinet: Client, { id }: { id: string }): Guild | null {
+	return (clinet.guilds.cache.get(id) ?? null);
 }
