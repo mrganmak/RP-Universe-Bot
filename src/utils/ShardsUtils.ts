41aa944e7ddcd41b5e4e64312f1e3cd172334ef0
mrganmak <mrganmak@mail.ru>
2023-12-15 01:00:19 +0300

    ShardsUtils: Added new shards funtions

--- src/utils/ShardsUtils.ts
+++ src/utils/ShardsUtils.ts
@@ -1,4 +1,4 @@
-import { Client, Guild, GuildEmoji } from "discord.js";
+import { Channel, Client, Guild, GuildEmoji, Message, MessageCreateOptions, Snowflake } from "discord.js";
 
 export function findEmojiInClient(clinet: Client, { id }: { id: string }): GuildEmoji | null {
 	return (clinet.emojis.cache.get(id) ?? null);
@@ -6,4 +6,25 @@ export function findEmojiInClient(clinet: Client, { id }: { id: string }): Guild
 
 export function findGuildInClient(clinet: Client, { id }: { id: string }): Guild | null {
 	return (clinet.guilds.cache.get(id) ?? null);
+}
+
+export function findChannelInClient(clinet: Client, { guildId, channelId }: { guildId: string, channelId: Snowflake }): Channel | null {
+	const guild = (clinet.guilds.cache.get(guildId) ?? null);
+	const channel = (guild ? (guild.channels.cache.get(channelId) ?? null) : null);
+
+	return channel;
+}
+
+export function sendMessageIntoChannel(clinet: Client, options: SendMessageIntoChannelOptions): Promise<Message> | null {
+	const guild = (clinet.guilds.cache.get(options.guildId) ?? null);
+	const channel = (guild ? (guild.channels.cache.get(options.channelId) ?? null) : null);
+
+	if (!channel?.isTextBased()) return null;
+	return channel.send(options.messageOptions);
+}
+
+interface SendMessageIntoChannelOptions {
+	guildId: string;
+	channelId: Snowflake;
+	messageOptions: MessageCreateOptions
 }
