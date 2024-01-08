aaf613bc60ab8b5f73dc624ada1f76db26108ca8
mrganmak <mrganmak@mail.ru>
2024-01-08 07:34:34 +0300

    refactor: change methods names

--- src/utils/PermissionsChecker.ts
+++ src/utils/PermissionsChecker.ts
@@ -1,20 +1,27 @@
 import { GuildChannel, GuildMember, PermissionResolvable, PermissionsBitField } from "discord.js";
 
 export class PermissionsChecker {
-	public static checkOnMissingPermissions(neededPermissions: PermissionResolvable[], targetMember: GuildMember, targetChannel: GuildChannel): boolean {
-		return (PermissionsChecker._getMissingPermissions(neededPermissions, targetMember, targetChannel).length > 0);
+	public static isMemberHasMissingPermissions(neededPermissions: PermissionResolvable[], targetMember: GuildMember): boolean {
+		return (PermissionsChecker._getMissingPermissions(neededPermissions, targetMember.permissions).length > 0);
 	}
 
-	public static getMissingPermissions(neededPermissions: PermissionResolvable[], targetMember: GuildMember, targetChannel: GuildChannel): ReturnType<PermissionsBitField['toArray']> {
-		return PermissionsChecker._getMissingPermissions(neededPermissions, targetMember, targetChannel);
+	public static getMemberMissingPermissions(neededPermissions: PermissionResolvable[], targetMember: GuildMember): ReturnType<PermissionsBitField['toArray']> {
+		return PermissionsChecker._getMissingPermissions(neededPermissions, targetMember.permissions);
 	}
 
-	private static _getMissingPermissions(neededPermissions: PermissionResolvable[], targetMember: GuildMember, targetChannel: GuildChannel): ReturnType<PermissionsBitField['toArray']> {
-		const permissionsForMember = targetChannel.permissionsFor(targetMember);
+	public static isMemberHasMissingPermissionsInChannel(neededPermissions: PermissionResolvable[], targetMember: GuildMember, targetChannel: GuildChannel): boolean {
+		return (PermissionsChecker._getMissingPermissions(neededPermissions, targetChannel.permissionsFor(targetMember)).length > 0);
+	}
+
+	public static getMissingPermissionsInChannel(neededPermissions: PermissionResolvable[], targetMember: GuildMember, targetChannel: GuildChannel): ReturnType<PermissionsBitField['toArray']> {
+		return PermissionsChecker._getMissingPermissions(neededPermissions, targetChannel.permissionsFor(targetMember));
+	}
+
+	private static _getMissingPermissions(neededPermissions: PermissionResolvable[], memberPermissions: PermissionsBitField): ReturnType<PermissionsBitField['toArray']> {
 		const missingPermissions: PermissionResolvable[] = [];
 
 		for (const permission of neededPermissions) {
-			if (!permissionsForMember.has(permission)) missingPermissions.push(permission);
+			if (!memberPermissions.has(permission)) missingPermissions.push(permission);
 		}
 
 		return new PermissionsBitField(missingPermissions).toArray();
