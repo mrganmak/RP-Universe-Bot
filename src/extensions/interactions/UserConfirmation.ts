59fe557a931a3555c448199e7f0f6fae63c778bc
mrganmak <mrganmak@mail.ru>
2024-01-08 07:37:58 +0300

    feat: added isNeedToEdit option

--- src/extensions/interactions/UserConfirmation.ts
+++ src/extensions/interactions/UserConfirmation.ts
@@ -2,32 +2,34 @@ import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, Replia
 import { TextsLocalizationsIds, getLocalizationForText, DEFAULT_SERVER_LANGUAGE, LocalizationsLanguages, userConfirmationInteractionButtonsSettings } from "../../index.js";
 
 class UserConfirmation {
-	public static async create(data: User | TextBasedChannel | Message | RepliableInteraction, options: UserConfirmationInteractionOptions): Promise<UserConfirmation> {
+	public static async create(data: User | TextBasedChannel | Message | RepliableInteraction, options: UserConfirmationInteractionOptions, isNeedToEdit: boolean = true): Promise<UserConfirmation> {
 		if (data instanceof User) {
 			const dmMessage = await data.send({
 				content: options.content,
 				embeds: options.embeds ?? [],
-				components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE)]
-			}).catch(() => { });
+				components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE, options)]
+			}).catch(console.error);
 			if (!dmMessage) throw new Error('Can\'t send message to this user');
 
 			return new this(dmMessage, options);
 		} else if (data instanceof Message) {
 			await data.fetch();
-			await data.edit({ components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE)] });
+			if (isNeedToEdit) await data.edit({ components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE, options)] });
 
 			return new this(data, options);
 		} else if (data instanceof BaseInteraction) {
-			if (data.replied || data.deferred) await data.editReply({
-				content: options.content,
-				embeds: options.embeds ?? [],
-				components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE)]
-			});
-			else await data.reply({
-				content: options.content,
-				embeds: options.embeds ?? [], 
-				components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE)]
-			});
+			if (isNeedToEdit) {
+				if (data.replied || data.deferred) await data.editReply({
+					content: options.content,
+					embeds: options.embeds ?? [],
+					components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE, options)]
+				});
+				else await data.reply({
+					content: options.content,
+					embeds: options.embeds ?? [], 
+					components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE, options)]
+				});	
+			}
 			const interactionMessage = await data.fetchReply();
 
 			return new this(data, options, interactionMessage);
@@ -36,22 +38,25 @@ class UserConfirmation {
 			const message = await data.send({
 				content: options.content,
 				embeds: options.embeds ?? [],
-				components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE)]
-			}).catch(() => { });
+				components: [this._createButtons(options.language ?? DEFAULT_SERVER_LANGUAGE, options)]
+			}).catch(console.error);
 			if (!message) throw new Error('Something went wrong');
 
 			return new this(message, options);
 		}
 	}
 
-	private static _createButtons(language: LocalizationsLanguages): ActionRowBuilder<ButtonBuilder> {
+	private static _createButtons(language: LocalizationsLanguages, options: UserConfirmationInteractionOptions): ActionRowBuilder<ButtonBuilder> {
 		const row = new ActionRowBuilder<ButtonBuilder>();
 
-		for (const buttonSettings of userConfirmationInteractionButtonsSettings) {
+		for (const buttonSettings of Object.values(userConfirmationInteractionButtonsSettings)) {
 			const button = new ButtonBuilder();
 			button
 				.setCustomId(buttonSettings.customId)
-				.setLabel(getLocalizationForText(buttonSettings.label, language))
+				.setLabel(getLocalizationForText(
+					(options.labels ? options.labels[buttonSettings.customId] : buttonSettings.label),
+					language
+				))
 				.setStyle(buttonSettings.style);
 			row.addComponents(button);
 		}
@@ -60,8 +65,8 @@ class UserConfirmation {
 	}
 
 	private _messageOrInteraction: Message | RepliableInteraction;
-	private _answer: UserAnswers | null = null;
-	private _buttonCollector!: InteractionCollector<ButtonInteraction>;
+	private _answer: UserConfirmationsAnswers | null = null;
+	private _buttonCollector: InteractionCollector<ButtonInteraction>;
 
 	private constructor(message: Message, options: UserConfirmationInteractionOptions)
 	private constructor(interaction: RepliableInteraction, options: UserConfirmationInteractionOptions, interactionMessage: Message)
@@ -69,11 +74,11 @@ class UserConfirmation {
 		this._messageOrInteraction = messageOrInteraction;
 
 		const message = ((messageOrInteraction instanceof Message) ? messageOrInteraction : interactionMessage) as Message;
-		this._buttonCollector = message.createMessageComponentCollector({ componentType: ComponentType.Button, filter: options.filter, time: options.time });
+		this._buttonCollector = message.createMessageComponentCollector({ componentType: ComponentType.Button, filter: options.filter, time: options.time, idle: 60*60*1000 });
 		this._buttonCollector.on('collect', (interaction) => (this._onCollect(interaction)));
 	}
 
-	public getUserAnswer(): Promise<UserAnswers> {
+	public getUserAnswer(): Promise<UserConfirmationsAnswers> {
 		return new Promise((resolve) => {
 			if (this._answer) return resolve(this._answer);
 			this._buttonCollector.on('collect', (interaction) => (resolve(interaction.customId === 'confirm' ? 'confirm' : 'deny')));
@@ -97,28 +102,31 @@ class UserConfirmation {
 	}
 }
 
-export async function getUserConfirmation(user: User, options: UserConfirmationInteractionOptions): Promise<UserAnswers>;
-export async function getUserConfirmation(channel: TextBasedChannel, options: UserConfirmationInteractionOptions): Promise<UserAnswers>;
-export async function getUserConfirmation(message: Message, options: UserConfirmationInteractionOptions): Promise<UserAnswers>;
-export async function getUserConfirmation(interaction: RepliableInteraction, options: UserConfirmationInteractionOptions): Promise<UserAnswers>;
-export async function getUserConfirmation(data: User | TextBasedChannel | Message | RepliableInteraction, options: UserConfirmationInteractionOptions): Promise<UserAnswers> {
-	const userConfirmation = await UserConfirmation.create(data, options);
+export async function getUserConfirmation(user: User, options: UserConfirmationInteractionOptions): Promise<UserConfirmationsAnswers>;
+export async function getUserConfirmation(channel: TextBasedChannel, options: UserConfirmationInteractionOptions): Promise<UserConfirmationsAnswers>;
+export async function getUserConfirmation(message: Message, options: UserConfirmationInteractionOptions, isNeedToEdit?: boolean): Promise<UserConfirmationsAnswers>;
+export async function getUserConfirmation(interaction: RepliableInteraction, options: UserConfirmationInteractionOptions, isNeedToEdit?: boolean): Promise<UserConfirmationsAnswers>;
+export async function getUserConfirmation(data: User | TextBasedChannel | Message | RepliableInteraction, options: UserConfirmationInteractionOptions, isNeedToEdit?: boolean): Promise<UserConfirmationsAnswers> {
+	const userConfirmation = await UserConfirmation.create(data, options, isNeedToEdit);
 
 	return await userConfirmation.getUserAnswer();
 }
 
-type UserAnswers = 'confirm' | 'deny';
+export type UserConfirmationsAnswers = 'confirm' | 'deny';
+export type UserConfirmationInteractionButtonsSettings = {
+	[key in UserConfirmationsAnswers]: UserConfirmationInteractionButtonSettings
+}
 export interface UserConfirmationInteractionButtonSettings {
 	label: TextsLocalizationsIds;
 	style: ButtonStyle;
-	customId: UserAnswers;
+	customId: UserConfirmationsAnswers;
 }
 
 interface UserConfirmationInteractionOptions extends MessageComponentCollectorOptions<ButtonInteraction> {
-	content: string;
+	content?: string;
 	language?: LocalizationsLanguages;
 	labels?: UserConfirmationButtonsLabels;
 	embeds?: EmbedBuilder[];
 }
 
-export type UserConfirmationButtonsLabels = Record<UserAnswers, TextsLocalizationsIds>;
+export type UserConfirmationButtonsLabels = Record<UserConfirmationsAnswers, TextsLocalizationsIds>;
