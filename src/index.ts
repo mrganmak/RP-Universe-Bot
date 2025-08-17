5c530618b665751ce7e6a65dc5d65cd3f3300c9f
mrganmak <mrganmak@mail.ru>
2025-08-17 03:39:14 +0300

    Refactor exports to use packages entry point
    Replaces individual exports in index.ts with a single export from './packages/index.js' to centralize and simplify module exports.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-export * from './enum.js'
-
-export * from './Databases/index.js'
-export * from './localizations/index.js'
-export * from './modules/index.js'
-export * from './extensions/index.js'
-export * from './types/index.js'
-export * from './utils/index.js'
-
-export * from './config.js'
-export * from './consts.js'
+export * from './packages/index.js';
