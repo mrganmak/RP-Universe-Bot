c207e5d59af02746bf08929ebbd55fe5db0f35fc
mrganmak <mrganmak@mail.ru>
2025-08-18 00:54:47 +0300

    Refactor module exports in index.ts
    Reorganized exports to use barrel files for bus and registry modules, and removed direct exports of specific files. This improves maintainability and simplifies import paths.

--- src/packages/modules/index.ts
+++ src/packages/modules/index.ts
@@ -1,5 +1,3 @@
-export * from './bus/Bus.js';
-export * from '../shared/util/Policies.js';
-export * from './bus/Guards.js';
+export * from './bus/index.js'
 export * from './types.js'
-export * from './bus/decorators.js'
+export * from './registry/index.js'
